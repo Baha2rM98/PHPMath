7a57dfd00194455a9bda74413a9cde4993617f25
Bahador <49520007+Baha2rM98@users.noreply.github.com>
2019-09-17 01:56:39 +0430

    test.php file updated

--- test.php
+++ test.php
@@ -8,11 +8,11 @@ use Math\Math;
 //example for Math class
 $math = new Math();
 echo '<pre>';
-print_r($math->primeNumbersList(20000, 185));
+print_r($math->ProbablePrimeNumbersList(20000, 185));
 echo '</pre>';
 
 //example for Algebra class
 $algebra = new Algebra();
 echo '<pre>';
-print_r($algebra->quadraticEquation(2, -3, 1));
+print_r($algebra->systemOf3Equation3anonymity([[2, -4, 5], [4, -1, 0], [-2, 2, -3]], [[-33], [-5], [19]]));
 echo '</pre>';
